4019442bb515d9d491549203e67e7adffec21a37
MatBRepo <broniek.mat@gmail.com>
2025-11-22 01:44:04 +0100

    part-5 UI UX Changes - 3

--- src/features/players/[id]/page.tsx
+++ src/features/players/[id]/page.tsx
@@ -2,13 +2,7 @@
 "use client";
 
 import PlayerEditorPage from "../PlayerEditorPage";
-// jeśli plik jest gdzie indziej, dostosuj ścieżkę, np.:
-// import PlayerEditorPage from "@/features/players/PlayerEditorPage";
 
-export default function PlayerDetailsRoute({
-  params,
-}: {
-  params: { id: string };
-}) {
-  return <PlayerEditorPage id={params.id} />;
+export default function Page() {
+  return <PlayerEditorPage />;
 }
